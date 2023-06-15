6d27dd37f65bc3ec2ee833d15e30e0db22d91e2a
ksahai <ksahai@opentext.com>
2023-06-15 19:06:45 +0530

    new changes

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const {merge}=require('webpack-merge');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const ModuleFederationPlugin=require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig=require('./webpack.common');
+const packageJson=require('../package.json');
 
 const devConfig={
     mode:'development',
@@ -17,14 +18,12 @@ const devConfig={
            remotes:{
             marketing:'marketing@http://localhost:8081/remoteEntry.js'
            },
-           shared:['react','react-dom']
+           shared:packageJson.dependencies
            // marketing on RHS is the "name" in the marketing module webpack
         //    The keys are the names of the different modules that we are going to try to require in or import in
         //    to our container project, and then the value will be where the remote entry file is for that module.
         }),
-        new HtmlWebpackPlugin({
-            template:'./public/index.html'
-        })
+        
     ]
 }
 
